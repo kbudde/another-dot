55b72313dc952c3322d3c766377723e08ae5ecae
Kris Budde <kris.budde@digits.schwarz>
2026-07-28 15:06:02 +0200

    add more tools

---
+++ dot_config/zim-plugins/stackit/init.zsh
@@ -0,0 +1,28 @@
+typeset -A cfg
+
+cfg=(
+    ["stackit"]="mise exec stackit -- stackit completion zsh"
+    #["switch"]="switch completion zsh"
+    #["env"]="production"
+)
+
+
+for key value in ${(kv)cfg}; do
+    () {
+        local path="$1"
+        local command="$2"
+        local gen_command="$3"
+
+        local compfile="$path/functions/_${command}"
+        [[ -z $command ]] && return 1
+
+        if [[ ! -e $compfile || $compfile -ot $(which $command) ]]; then
+            eval $gen_command >| $compfile
+            print -u2 -PR "* Detected a new version '$command'. Regenerated completions."
+        fi
+
+        #compdef(){}
+        source $compfile
+        #unset compdef
+    }  ${0:h} "$key" "$value"
+done
