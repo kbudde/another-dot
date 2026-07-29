6924f73fda221ab0bd4adb13e344b10fc0632b97
Kris Budde <kris@budd.ee>
2026-07-29 23:27:18 +0200

    optimize completions

---
+++ dot_config/zim-plugins/allCompletions/init.zsh
@@ -0,0 +1,29 @@
+typeset -A cfg
+
+cfg=(
+    ["crane"]='crane completion zsh'
+    ["talosctl"]="talosctl completion zsh"
+    ["stackit"]="stackit completion zsh"
+    ["k9s"]="k9s completion zsh"
+    ["starship"]="starship completions zsh"
+    ["lima"]="limactl completion zsh"
+    ["argocd"]="argocd completion zsh"
+    ["just"]="just --completions zsh"
+    ["kubara"]="kubara completion zsh"
+    ["jira"]="jira completion zsh"
+    ["myks"]=" myks completion zsh"
+)
+
+() {
+    for command gen_command in ${(kv)cfg}; do
+        local compfile="$1/functions/_${command}"
+        [[ -z $command ]] && return 1
+        if [[ ! -e $compfile || $compfile -ot $(which $command) ]]; then
+            eval $gen_command >| $compfile
+            print -u2 -PR "* Detected a new version '$command'. Regenerated completions."
+        fi
+    done
+}  ${0:h} 
+
+
+fpath=("$zmodule" $fpath)
