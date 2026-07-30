284de3ab5acbeec50f926b26a2992da80052e85b
Kris Budde <kris.budde@digits.schwarz>
2026-07-30 08:19:17 +0200

    clean up empty completion files (in case something went wrong

--- dot_config/zim-plugins/allCompletions/init.zsh
+++ dot_config/zim-plugins/allCompletions/init.zsh
@@ -11,13 +11,14 @@ cfg=(
     ["just"]="just --completions zsh"
     ["kubara"]="kubara completion zsh"
     ["jira"]="jira completion zsh"
-    ["myks"]=" myks completion zsh"
+    ["myks"]="myks completion zsh"
 )
 
 () {
     for command gen_command in ${(kv)cfg}; do
         local compfile="$1/functions/_${command}"
         [[ -z $command ]] && return 1
+        [ -s $compfile ] || rm "$compfile" # clean up empty file
         if [[ ! -e $compfile || $compfile -ot $(which $command) ]]; then
             eval $gen_command >| $compfile
             print -u2 -PR "* Detected a new version '$command'. Regenerated completions."
